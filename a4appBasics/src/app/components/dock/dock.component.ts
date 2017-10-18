75701823838f36b0a308fa4fe7ceef74a2a0834f
Chaudhary <anoop.chaudhary@thermofisher.com>
2017-10-18 10:31:02 +0530

    ag-grid

--- a4appBasics/src/app/components/dock/dock.component.ts
+++ a4appBasics/src/app/components/dock/dock.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-dock',
@@ -10,10 +11,12 @@ export class DockComponent implements OnInit {
   total:number;
   button1:boolean;
   button2:boolean;
-  constructor() { this.total=0;this.button1=true;this.button2=true;}
+  constructor( private _router: Router ) {
+    this.total=0;this.button1=true;this.button2=true;}
 
   ngOnInit() {
     console.log("ngOnInitCalled");
+    this.myFunction();
     this.personList=[{firstName:'Anoop',lastName:'Chaudhary',age:24},{firstName:'Atul',lastName:'Sachan',age:23},{firstName:'Ajay',lastName:'Mohan',age:23}];
   }
 
@@ -28,6 +31,7 @@ export class DockComponent implements OnInit {
   }
   
   myFunction() {
+    //this._router.navigate(['']);
     var myWindow = window.open("/", "", "width=200,height=100");
 }
 
